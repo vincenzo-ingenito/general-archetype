b85a7e17778cafa4c59644b81680e45509c7660c
Vincenzo Ingenito <54367888+vincenzo-ingenito@users.noreply.github.com>
2023-02-27 22:19:52 +0100

    feat: First commit

---
+++ src/main/java/it/finanze/sanita/fse/generalarchetype/config/mongo/MongoPropertiesCFG.java
@@ -0,0 +1,23 @@
+/*
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+package it.finanze.sanita.fse.generalarchetype.config.mongo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+
+/**
+ *	Mongo properties configuration.
+ */
+@Data
+@Component
+@EqualsAndHashCode(callSuper = false)  
+public class MongoPropertiesCFG {
+  
+	@Value("${data.mongodb.uri}")
+	private String uri;
+}
---
+++ src/test/java/it/finanze/sanita/fse/generalarchetype/GeneralArchetypeApplicationTests.java
@@ -0,0 +1,13 @@
+package it.finanze.sanita.fse.generalarchetype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneralArchetypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
