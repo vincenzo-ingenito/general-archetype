530ee27691f45d767b2822d0013d464450febc81
Vincenzo Ingenito <Vincenzo.Ingenito-CIC@ibm.com>
2023-11-01 10:50:28 +0100

    feat: Update archetype

---
+++ target/classes/archetype-resources/src/main/java/config/mongo/MongoDatabaseCFG.java
@@ -0,0 +1,51 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+/*
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+package ${package}.config.mongo;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+ 
+
+/**
+ *	Configuration for MongoDB.
+ */
+@Configuration
+public class MongoDatabaseCFG {
+
+    final List<Converter<?, ?>> conversions = new ArrayList<>();
+
+    @Bean
+    public MongoDatabaseFactory mongoDatabaseFactory(final MongoPropertiesCFG mongoPropertiesCFG){
+        return new SimpleMongoClientDatabaseFactory(mongoPropertiesCFG.getUri());
+    }
+
+    @Bean
+    @Primary
+    public MongoTemplate mongoTemplate(final MongoDatabaseFactory factory, final ApplicationContext appContext) {
+        final MongoMappingContext mongoMappingContext = new MongoMappingContext();
+        mongoMappingContext.setApplicationContext(appContext);
+        MappingMongoConverter converter = new MappingMongoConverter(new DefaultDbRefResolver(factory), mongoMappingContext);
+        converter.setTypeMapper(new DefaultMongoTypeMapper(null));
+        return new MongoTemplate(factory, converter);
+    }
+  
+ 
+}
